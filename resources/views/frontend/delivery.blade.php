b3edb323a0016c375266de1736431a5bd4a6d514
thienkimlove <thienkimlove@gmail.com>
2016-07-28 00:20:18 +0700

    update

--- resources/views/frontend/delivery.blade.php
+++ resources/views/frontend/delivery.blade.php
@@ -12,20 +12,7 @@
                     <li class="active">Phân phối</li>
                 </ul>
 
-                @foreach ($totalDeliveries as $area => $cities)
-                    <div class="title01">{{$area}}</div>
-                    <div class="data">
-                        @foreach ($cities->chunk(6) as $partCities)
-                            <article class="item">
-                                @foreach ($partCities as $city)
-                                    <a href="{{url('phan-phoi/'. $city->id)}}" title="">{{config('delivery')['city'][$city->city]}}</a>
-                                @endforeach
-                            </article>
-                        @endforeach
-                    </div>
-                @endforeach
-
-
+             
                 <div class="boxDist">
                     @foreach ($totalDeliveries as $area => $cities)
                     <div class="data clearFix">
