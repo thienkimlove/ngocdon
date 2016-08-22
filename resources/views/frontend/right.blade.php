0c162fa3de89c7e4d8aa3f0396328520b47257b1
Manh Quan Do <manhquan.do@ved.com.vn>
2016-08-22 05:08:49 +0000

    update

--- resources/views/frontend/right.blade.php
+++ resources/views/frontend/right.blade.php
@@ -43,7 +43,7 @@
             <div class="fb-page" data-href="https://www.facebook.com/AlidSlim-Eo-thon-d%C3%A1ng-%C4%91%E1%BA%B9p-1025739557533863/" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true"><blockquote cite="https://www.facebook.com/AlidSlim-Eo-thon-d%C3%A1ng-%C4%91%E1%BA%B9p-1025739557533863/" class="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/AlidSlim-Eo-thon-d%C3%A1ng-%C4%91%E1%BA%B9p-1025739557533863/">AlidSlim - Eo thon, dáng ð?p</a></blockquote></div>
         </div>
     </div>
-    @if (!in_array($page, ['lien-he', 'phan-phoi']))
+    @if ($rightNews && !in_array($page, ['lien-he', 'phan-phoi']))
         <div class="boxNews" id="sidebar">
         <h3 class="globalTitle">
             Tin nổi bật
